6f89430efbbf080fe87bc8a588d14a7a6d390c68
EnoLA-013 <zakiasultana8801@gmail.com>
2025-08-22 18:43:02 +0600

    second commit

--- script.js
+++ script.js
@@ -66,3 +66,29 @@ var swiper = new Swiper(".product_slider", {
     },
   },
 });
+
+
+
+var swiper = new Swiper(".review_slider", {
+  loop:true,
+  spaceBetween: 20,
+
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+      
+    },
+    1020: {
+      slidesPerView: 3,
+      
+    },
+  },
+});
